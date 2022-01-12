c8176460732844aef58deadb996bf7bf93722ea9
katrav05 <97584188+katrav05@users.noreply.github.com>
2022-01-12 12:17:14 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import NamshiInterviewApplication from './component/NamshiInterviewApplication';
+function App() {
+  return (
+    <div className="Box">
+      <NamshiInterviewApplication/>
+    </div>
+  );
+}
+
+export default App;
